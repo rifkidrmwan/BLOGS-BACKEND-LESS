d029dddedf5f1d42e3431328e4b308d290788a6b
rifkidrmwan <100209260+rifkidrmwan@users.noreply.github.com>
2026-03-11 15:50:25 +0700

    feat: add login page (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,9 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
+  },{
+    path: "/login",
+    element: <Login/>,
   },
 ]);
 
